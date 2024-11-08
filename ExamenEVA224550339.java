c2b61e6a93ad2cbddb80339c420fd24e5e3e5f49
JosueLin-HUB <L24550339@chihuahua2.tecnm.mx>
2024-11-08 17:02:25 -0600

    Add files via upload

---
+++ ExamenEVA224550339.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examenpracticoeva2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author elfor
+ */
+public class ExamenPracticoEVA2 {
+
+    public static void main(String[] args) {
+        
+        String compu;
+        int huma;
+        
+        Scanner captu = new Scanner(System.in);
+        
+        for(int i = 0;;i++){
+          System.out.println("piedra(1), papel(2) o tijera(3).....");
+        huma=captu.nextInt();
+        if (huma >3)
+        break;
+        }
+        if(huma == 1)
+            System.out.println("Piedra");
+        if(huma == 2)
+            System.out.println("Papel");
+        if(huma == 3)
+            System.out.println("Tijera");
+        
+        
+        double valor = Math.random();
+        if(valor<.3)
+        
+        
+            }
+}
+  
+        
+        
+            
+        
+       
+        
+        
+        
+        
+        
+ 
+
